5e6914c06ce5c9e35a8363e3562211e2eb1271a1
iiffd <sml.hng@gmail.com>
2018-04-09 16:47:52 -0700

    Add prompt library for user input

--- run.js
+++ run.js
@@ -1,20 +1,30 @@
-const board = (positions) => `  
+const prompt = require('prompt');
+
+const board = (positions, player) => `  
     ${positions[0]} | ${positions[1]} | ${positions[2]}
-   --------
+   -----------
     ${positions[3]} | ${positions[4]} | ${positions[5]}
-   ________
+   -----------
     ${positions[6]} | ${positions[7]} | ${positions[8]}
     
-  player x move:
+  player ${player} move:
 `
 
 class Game {
   constructor() {
-    this.positions = Array(9).fill('');
+    this.positions = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
+    this.player = 'X';
   }
 
   generateBoard() {
-    console.log(board(this.positions));
+    console.log(board(this.positions, this.player));
+    prompt.start();
+
+    prompt.get(['move'], (err, result) => {
+      this.positions[Number(result.move)] = this.player;
+      this.player = this.player === 'X' ? 'O' : 'X';
+      this.generateBoard();
+    });
   }
 
 }
