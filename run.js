ed6f0cd454dff31ace1c6069ab024f7d9190b5ce
iiffd <sml.hng@gmail.com>
2018-04-09 16:29:55 -0700

    Make basic outline for game class

--- run.js
+++ run.js
@@ -1,11 +1,24 @@
-const board = `  
-      |  |
-    --------
-      |  |
-    ________
-      |  |
+const board = (positions) => `  
+    ${positions[0]} | ${positions[1]} | ${positions[2]}
+   --------
+    ${positions[3]} | ${positions[4]} | ${positions[5]}
+   ________
+    ${positions[6]} | ${positions[7]} | ${positions[8]}
     
   player x move:
 `
 
-console.log(board);
+class Game {
+  constructor() {
+    this.positions = Array(9).fill('');
+  }
+
+  generateBoard() {
+    console.log(board(this.positions));
+  }
+
+}
+
+const game = new Game();
+
+console.log(game.generateBoard());
