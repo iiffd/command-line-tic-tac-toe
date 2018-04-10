2519458ffb4c479cadd42b831bed22e2765c9e92
iiffd <sml.hng@gmail.com>
2018-04-09 17:17:16 -0700

    Check for win and draw

--- run.js
+++ run.js
@@ -8,7 +8,18 @@ const board = (positions, player) => `
     ${positions[6]} | ${positions[7]} | ${positions[8]}
     
   player ${player} move:
-`
+  `
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
 class Game {
   constructor() {
@@ -22,11 +33,32 @@ class Game {
 
     prompt.get(['move'], (err, result) => {
       this.positions[Number(result.move)] = this.player;
+      
+      if (this.evaluate(this.player)) {
+        console.log(`player ${this.player} wins!!!`);
+        return;
+      } else if (this.checkDraw()) {
+        console.log('DRAW');
+        return;
+      }
+      
       this.player = this.player === 'X' ? 'O' : 'X';
       this.generateBoard();
     });
   }
 
+  evaluate(char) {
+    let bool = false;
+    winConditions.forEach((win) => {
+      if (win.every((index) => this.positions[index] === char)) bool = true;
+    })
+
+    return bool;
+  }
+
+  checkDraw() {
+    return this.positions.every((char) => char === 'X' || char === 'O');
+  }
 }
 
 const game = new Game();
